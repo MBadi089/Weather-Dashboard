04cae1f94b7a617cbf6da579205727ec3018b1bf
Marcos <marcosbadillo089@gmail.com>
2021-05-01 18:20:04 -0500

    added fetch 1 and 2

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,10 +1,71 @@
-//API key set as const to be called on without changing it
-const APIKey ="40673f276cf44bc5a316f589061aceb6";
+//--------------------Global Variables Section---------------------//
+var cityInput;
+var city;
+var temp;
+var fahrenheit;
+var wind;
+var humidity;
+var cityLatitude;
+var cityLongitude;
+var lat;
+var lon;
+var searchBtn = document.getElementById('searchBtn');
+searchBtn.addEventListener("click", getCityInput);
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=midland&appid=" + APIKey)
+//API key set as const to be called on without changing it
+const apiKey ="40673f276cf44bc5a316f589061aceb6";
+
+//-----------------End of Global Variables Section-----------------//
+
+
+//----------------------getCityInput Section-----------------------//
+//This function will get the user's choice of city to search for    
+    function getCityInput() {
+      cityInput = document.querySelector("#userInput").value;
+
+      
+
+//First API fetch to get the current day weather from user's input
+      fetch("https://api.openweathermap.org/data/2.5/weather?q="+ cityInput + "&appid=" + apiKey)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
       console.log(data);
+
+//this grabs the city lat and lon from user's input
+      cityLatitude = data.coord.lat;
+      cityLongitude = data.coord.lon;
+      console.log(cityLatitude);
+      console.log(cityLongitude);
+
+      city = document.querySelector('#city').innerHTML='City: ' + cityInput;
+
+      //converting kelvin to fahrenheit
+      kelvinToFahrenheit = (Math.round(data.main.temp - 273.15) * 9 / 5 + 32);
+      temp = document.querySelector("#temp").innerHTML='Temp: ' + kelvinToFahrenheit; //data.main.temp;
+
+      wind = document.querySelector("#wind").innerHTML='Wind: '+ data.wind.speed;
+      humidity = document.querySelector("#humidity").innerHTML='Humidity: '+ data.main.humidity;
     });
+      
+    //Second API fetch, this is to get the UV Index
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLatitude +"&lon=" + cityLongitude + "&appid=" + apiKey )//33.44&lon=-94.04&appid="+ apiKey)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+    console.log('data: ',data);
+    });
+
+  }
+  
+//------------------Enod of getCityInput Section-------------------//
+
+
+//----------------------getAPISource Section-----------------------//
+//This function will get the API source
+function getAPISource() {
+
+}
+//-------------------End of getAPISource Section-------------------//
